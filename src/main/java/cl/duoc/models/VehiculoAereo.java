cd25ba4db3a7620da50225f40f58d1c6895ff172
DanielAranedaM <d.aranedam@duocuc.cl>
2022-10-28 23:01:22 -0300

    Cambio clases - Daniel

--- src/main/java/cl/duoc/models/VehiculoAereo.java
+++ src/main/java/cl/duoc/models/VehiculoAereo.java
@@ -1,13 +1,54 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package cl.duoc.models;
 
-/**
- *
- * @author danar
- */
+import java.time.LocalDate;
+
 public class VehiculoAereo {
+    private String Nombre;
+    private int HorasVoladas;
+    private boolean ListoParaVolar;
+    private LocalDate FechaUltimoVuelo;
+
+    public VehiculoAereo() {
+        this.Nombre = "";
+        this.HorasVoladas = 0;
+        this.ListoParaVolar = false;
+        this.FechaUltimoVuelo = LocalDate.now();
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public int getHorasVoladas() {
+        return HorasVoladas;
+    }
+
+    public void setHorasVoladas(int HorasVoladas) {
+        this.HorasVoladas = HorasVoladas;
+    }
+
+    public boolean isListoParaVolar() {
+        return ListoParaVolar;
+    }
+
+    public void setListoParaVolar(boolean ListoParaVolar) {
+        this.ListoParaVolar = ListoParaVolar;
+    }
+
+    public LocalDate getFechaUltimoVuelo() {
+        return FechaUltimoVuelo;
+    }
+
+    public void setFechaUltimoVuelo(LocalDate FechaUltimoVuelo) {
+        this.FechaUltimoVuelo = FechaUltimoVuelo;
+    }
+    
+    
+    
     
 }
