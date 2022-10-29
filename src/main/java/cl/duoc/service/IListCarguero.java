a03169750461f5e08e9638356979dbe10875cbe7
DanielAranedaM <d.aranedam@duocuc.cl>
2022-10-29 00:26:00 -0300

    interfaz carguero

--- src/main/java/cl/duoc/service/IListCarguero.java
+++ src/main/java/cl/duoc/service/IListCarguero.java
@@ -1,6 +1,13 @@
 
 package cl.duoc.service;
 
+import cl.duoc.models.Carguero;
+
 public interface IListCarguero {
-    
+    public void Guardar ( Carguero carguero );
+    public void Reemplazar (int indice, Carguero carguero);
+    public void Eliminar (int id);
+    public void Eliminar (Carguero carguero);
+    public void Listar();
+    public void Listar (int index);
 }
